4d2991094f257f04fcd1d8c70c93b0babafb5ae1
udaytejeshwar <67693720+udaytejeshwar@users.noreply.github.com>
2025-07-08 19:35:32 -0400

    Update Brands.tsx

--- src/sections/Brands.tsx
+++ src/sections/Brands.tsx
@@ -45,7 +45,8 @@ const Brands: React.FC = () => {
                 <h3 className="text-1xl sm:text-2xl md:text-3xl font-thin tracking-[0.20em] mb-3 sm:mb-4 leading-tight uppercase text-white"
   style={{ fontFamily: "'Nunito Sans', sans-serif", fontStretch: 'expanded' }}
 >
-  ARK SPINDLES<span className="text-[0.5em] align-top ml-[0.1em]">™</span></h3>
+  ARK SPINDLES<span className="text-[0.5em] ml-[0.1em]"
+    style={{ verticalAlign: 'super' }}>™</span></h3>
               </div>
             </div>
             
